d6d5995425f2d9348857b0b2ee17b591ed9729a5
David Yang <didalfn88@gmail.com>
2020-08-18 17:40:59 +0900

    Updates

---
+++ precache-manifest.02768b168241dd79bff2f85615612673.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a7be6beafd68011f4aa7f43ff5ee7c9",
+    "url": "/metflix/index.html"
+  },
+  {
+    "revision": "cd206f5f96537cedbba9",
+    "url": "/metflix/static/js/2.2dc3eaf6.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/metflix/static/js/2.2dc3eaf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "751a498e5722218a64dd",
+    "url": "/metflix/static/js/main.f56f9619.chunk.js"
+  },
+  {
+    "revision": "a1a1805acc4ab7d1eb5d",
+    "url": "/metflix/static/js/runtime-main.23033591.js"
+  },
+  {
+    "revision": "5725b4c466ca3726e7a6df52730dacd2",
+    "url": "/metflix/static/media/noPosterSmall.5725b4c4.png"
+  }
+]);
